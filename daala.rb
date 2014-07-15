cb4cfcc12914c6ec22dde54c8486403e422e7700
Ralph Giles <giles@mozilla.com>
2014-07-14 19:06:51 -0700

    daala: initial head-only formula
    Daala is an experimental video codec. No stable releases yet,
    but it's nice to have a quick way to install the current code
    for testing.

---
+++ daala.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class Daala < Formula
+  homepage 'https://xiph.org/daala/'
+
+  head do
+    url 'https://git.xiph.org/daala.git'
+
+    depends_on 'pkg-config' => :build
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+  end
+
+  depends_on 'libpng'
+  depends_on 'jpeg'
+  depends_on 'libogg'
+  depends_on 'sdl'
+  depends_on 'check'
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-doc",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
